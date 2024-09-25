fa88b394411895792e1e43467b301d10074dd288
Divine Sofowora <w0835525@myscc.ca>
2024-09-24 21:21:54 -0400

    Created student and student list components

---
+++ src/app/student-list-item/student-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-list-item.component.html',
+  styleUrl: './student-list-item.component.scss'
+})
+export class StudentListItemComponent {
+
+}
