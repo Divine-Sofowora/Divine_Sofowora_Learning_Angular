63a0b8c1048fee5e911f7db9eeb3dfed0e0ad7d4
Divine Sofowora <w0835525@myscc.ca>
2024-09-24 22:04:10 -0400

    updated

--- src/app/student-list-item/student-list-item.component.ts
+++ src/app/student-list-item/student-list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { StudentListComponent } from '../student-list/student-list.component';
 
 @Component({
   selector: 'app-student-list-item',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './student-list-item.component.scss'
 })
 export class StudentListItemComponent {
-
+  @Input()  student?: StudentListComponent;
 }
