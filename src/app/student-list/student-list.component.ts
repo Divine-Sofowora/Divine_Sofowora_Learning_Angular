a683dcae49329621b2c8993d239c9c3539d51922
Divine Sofowora <w0835525@myscc.ca>
2024-09-30 11:44:51 -0400

    Assignment completed

--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -2,12 +2,12 @@ import {NgClass, NgFor} from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { User } from '../Shared/Models/user';
-import { StudentListItemComponent } from '../student-list-item/student-list-item.component';
+import { StudentListItemComponent } from '../student-list-item/student-list-item.component'
 
 @Component({
   selector: 'app-student-list',
   standalone: true,
-  imports: [FormsModule, StudentListComponent, NgFor, StudentListItemComponent, NgClass],
+  imports: [FormsModule, NgFor, NgClass, StudentListItemComponent],
   templateUrl: './student-list.component.html',
   styleUrl: './student-list.component.scss'
 })
