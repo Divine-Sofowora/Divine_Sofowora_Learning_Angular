488fd9e13a304778909a89e81aae6822b56ca53c
Divine Sofowora <w0835525@myscc.ca>
2024-10-02 19:32:40 -0400

    Generated the service folders

--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -14,11 +14,4 @@ import { StudentListItemComponent } from '../student-list-item/student-list-item
 
 export class StudentListComponent {
 
-  userList : User[]= [
- {  studentId: 1, studentName: "Matt", studentEmail: "Haug@gmail.com", phone: "309-3739-1353", isPresent: false},
- { studentId: 2, studentName: "Sarah", studentEmail: "Sarah@gmail.com", phone: "282-2722-2322", isPresent: true },
- { studentId: 3, studentName: "James", studentEmail: "James@yahoo.com", phone: "4749-4644-4494", isPresent: false },
- { studentId: 4, studentName: "Emily", studentEmail: "Emily@hotmail.com", phone: "459-4525-5115", isPresent: true },
-];
-
 }
