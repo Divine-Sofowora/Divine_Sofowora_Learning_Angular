6a949398e6ccf8aabc373ca0df8672aa28d40756
Divine Sofowora <w0835525@myscc.ca>
2024-10-02 20:30:37 -0400

    Added new service using dependency injection

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import {Observable, of} from "rxjs";
+import {User} from "../Shared/Models/user"
+import {userList} from "../Shared/Models/mockData-user";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,9 @@ import { Injectable } from '@angular/core';
 export class UserService {
 
   constructor() { }
+
+
+  getUser(): Observable<User[]>{
+    return  of (userList)
+  }
 }
