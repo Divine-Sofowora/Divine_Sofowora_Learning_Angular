75d790f79e6a593fe9ad78e172b34833313606b7
Divine Sofowora <w0835525@myscc.ca>
2024-10-08 19:31:33 -0400

    Created routers

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter, Routes} from "@angular/router";
+import {StudentListComponent} from "./app/student-list/student-list.component";
+import {StudentListItemComponent} from "./app/student-list-item/student-list-item.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+const routes: Routes =[
+  { path: 'student', component: StudentListComponent },
+  { path: 'student/:id', component: StudentListItemComponent },
+]
+
+bootstrapApplication(AppComponent, {
+  providers:[provideRouter(routes)]
+});
+
+
